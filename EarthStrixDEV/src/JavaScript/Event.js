874b875e100be7e50a3b632c179d53bd6be29f16
warapon6430300862 <warapon.jitsook@gmail.com>
2022-04-10 22:56:00 +0700

    Update 10/4/22

---
+++ EarthStrixDEV/src/JavaScript/Event.js
@@ -0,0 +1,32 @@
+var btn = document.querySelectorAll('button');
+var name_category = document.getElementById('name-category');
+var name_user = document.getElementById("name-user");
+
+console.log(name_category);
+console.log(name_user);
+
+btn[0].addEventListener('click', () => {
+    let getdata_prompt = prompt("Enter your name");
+    if (getdata_prompt != "")
+    {
+        name_user.innerText = `Welcome back, ${ getdata_prompt.toUpperCase() }`;
+    } else
+    {
+        let getdata_prompt = prompt("Enter your name", "Enter your name before submit!!!");
+    }
+})
+
+btn[1].addEventListener('click', () => {
+    let name_text = "Web Development";
+    name_category.innerText = `Course : ${ name_text }`;
+})
+
+btn[2].addEventListener('click', () => {
+    let name_text = "Programming Language";
+    name_category.innerText = `Course : ${name_text}`;
+})
+
+btn[3].addEventListener('click', () => {
+    let name_text = "Mobile Development";
+    name_category.innerText = `Course : ${name_text}`;
+})
