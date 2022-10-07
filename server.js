e296867f6715f5852c99c9a518015e69a9df3cc0
warapon6430300862 <warapon.jitsook@gmail.com>
2022-10-07 20:15:12 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const path = require('path');
 const app = express();
 const port = 5000
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(express.static(path.join(__dirname, './frontend')));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../frontend/views'));
+app.set('views', path.join(__dirname, './frontend/views'));
 app.use(router);
 
 app.listen(port, () => console.log(`Server hosting on port ${port}`))
